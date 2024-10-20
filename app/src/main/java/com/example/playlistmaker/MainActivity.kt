831ad5994e15afc7427a2f21ef01842a99041c75
Vlad <vladismann@yandex.ru>
2024-10-20 19:50:55 +0300

    feat: button click

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+         fun showToast(message: String) {
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        }
+
+        val buttonSearch = findViewById<Button>(R.id.search)
+        val buttonMedia = findViewById<Button>(R.id.media)
+        val buttonSettings = findViewById<Button>(R.id.settings)
+
+        val buttons = listOf(buttonSearch, buttonMedia, buttonSettings)
+
+        buttons.forEach { button ->
+            button.setOnClickListener {
+                showToast("Кнопка нажата")
+            }
+        }
     }
 }
