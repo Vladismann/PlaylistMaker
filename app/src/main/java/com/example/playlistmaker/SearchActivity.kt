71491d9788618f882ee74f768bdde8279e38ab3d
Vlad <vladismann@yandex.ru>
2024-11-06 20:58:42 +0300

    feat: сохранение и возврат данных из инпута при прерывании

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -12,13 +12,19 @@ import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
+    companion object {
+        private const val INPUT_KEY = "ACTUAL_TEXT"
+    }
+    private lateinit var actualInput: String
+    private lateinit var inputEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
         val buttonBack = findViewById<ImageView>(R.id.go_back)
-        val inputEditText = findViewById<EditText>(R.id.input_search)
         val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        inputEditText = findViewById<EditText>(R.id.input_search)
 
         buttonBack.setOnClickListener {
             val displayIntent = Intent(this, MainActivity::class.java)
@@ -38,6 +44,7 @@ class SearchActivity : AppCompatActivity() {
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 clearButton.visibility = clearButtonVisibility(s)
+                actualInput = s.toString()
             }
 
             override fun afterTextChanged(s: Editable?) {
@@ -55,4 +62,15 @@ class SearchActivity : AppCompatActivity() {
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(INPUT_KEY, actualInput)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        actualInput = savedInstanceState.getString(INPUT_KEY, "")
+        inputEditText.setText(actualInput)
+        inputEditText.setSelection(actualInput.length)
+    }
 }
