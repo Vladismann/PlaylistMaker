3cae00598371bd746696028ca0ee2883cd4b2cf9
Vlad <vladismann@yandex.ru>
2024-12-03 20:05:35 +0300

    feat: Реализован поиск треков в iTunes

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -8,8 +8,11 @@ import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.isVisible
@@ -39,19 +42,31 @@ class SearchActivity : AppCompatActivity() {
 
     private var actualInput: String = ""
     private lateinit var inputEditText: EditText
+    private lateinit var tracks: MutableList<Track>
+    private lateinit var errorElement: LinearLayout
+    private lateinit var refreshButton: Button
+    private lateinit var errorImage: ImageView
+    private lateinit var errorText: TextView
+    private lateinit var trackAdapter: TrackAdapter
+    private lateinit var rvTrack: RecyclerView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        val clearButton = findViewById<ImageView>(R.id.clearIcon)
-        inputEditText = findViewById(R.id.input_search)
         setSupportActionBar(toolbar)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
 
-        var tracks: MutableList<Track> = arrayListOf()
-        val trackAdapter = TrackAdapter(tracks)
-        val rvTrack = findViewById<RecyclerView>(R.id.rvTrack)
+        inputEditText = findViewById(R.id.input_search)
+        errorElement = findViewById(R.id.errorMessageElement)
+        refreshButton = findViewById(R.id.refreshTracks)
+        errorImage = findViewById(R.id.errorImage)
+        errorText = findViewById(R.id.errorText)
+        tracks = arrayListOf()
+        trackAdapter = TrackAdapter(tracks)
+        rvTrack = findViewById(R.id.rvTrack)
         rvTrack.adapter = trackAdapter
 
         toolbar.setNavigationOnClickListener {
@@ -62,6 +77,7 @@ class SearchActivity : AppCompatActivity() {
             inputEditText.setText("")
             trackAdapter.clear()
             rvTrack.visibility = View.GONE
+            errorElement.visibility = View.GONE
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
         }
@@ -80,30 +96,55 @@ class SearchActivity : AppCompatActivity() {
                 // empty
             }
         }
-        inputEditText.addTextChangedListener(simpleTextWatcher)
 
+        inputEditText.addTextChangedListener(simpleTextWatcher)
         inputEditText.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
-                apiService.searchTracks(actualInput).enqueue(object : Callback<TrackResponse> {
-                    override fun onResponse(
-                        call: Call<TrackResponse>,
-                        response: Response<TrackResponse>
-                    ) {
-                        if (response.isSuccessful && response.body() != null) {
-                            tracks = response.body()!!.results
-                            trackAdapter.updateData(tracks)
-                            rvTrack.visibility = View.VISIBLE
-                        }
-                    }
-
-                    override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
-                        Log.e("MainActivity", "Error: ${t.message}")
-                    }
-                })
+                searchTracks()
+                true
             }
-            true
+            false
         }
 
+        refreshButton.setOnClickListener {
+            searchTracks()
+        }
+    }
+
+    private fun searchTracks() {
+        apiService.searchTracks(actualInput).enqueue(object : Callback<TrackResponse> {
+            override fun onResponse(
+                call: Call<TrackResponse>,
+                response: Response<TrackResponse>
+            ) {
+
+                if (response.isSuccessful && response.body() != null) {
+                    tracks = response.body()!!.results
+                    if (tracks.isEmpty()) {
+                        trackAdapter.clear()
+                        rvTrack.visibility = View.GONE
+                        errorElement.visibility = View.VISIBLE
+                        errorImage.setImageResource(R.drawable.track_not_found)
+                        errorText.setText(R.string.nothing_found)
+                        refreshButton.visibility = View.GONE
+                        return
+                    }
+                    trackAdapter.updateData(tracks)
+                    rvTrack.visibility = View.VISIBLE
+                    errorElement.visibility = View.GONE
+                }
+            }
+
+            override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                trackAdapter.clear()
+                rvTrack.visibility = View.GONE
+                errorElement.visibility = View.VISIBLE
+                errorImage.setImageResource(R.drawable.track_search_error)
+                errorText.setText(R.string.connection_error)
+                refreshButton.visibility = View.VISIBLE
+                Log.e("Retrofit", "Request failed", t)
+            }
+        })
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
