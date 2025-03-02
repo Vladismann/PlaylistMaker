f5d828b17e2c2ac09730b13faa0a9fc0d81c7723
Vlad <vladismann@yandex.ru>
2025-03-02 22:52:28 +0300

    refactor: правка багов

--- app/src/main/java/com/example/playlistmaker/player/ui/TrackActivity.kt
+++ app/src/main/java/com/example/playlistmaker/player/ui/TrackActivity.kt
@@ -1,16 +1,17 @@
 package com.example.playlistmaker.player.ui
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.ActivityAudioplayerBinding
 import com.example.playlistmaker.player.view_model.PlayerState
 import com.example.playlistmaker.player.view_model.TrackScreenState
 import com.example.playlistmaker.player.view_model.TrackViewModel
 
-class TrackActivity : ComponentActivity() {
+class TrackActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAudioplayerBinding
     private val viewModel by viewModels<TrackViewModel> {
@@ -56,6 +57,7 @@ class TrackActivity : ComponentActivity() {
     private fun loadTrackInfo(screenState: TrackScreenState.Content) {
         Glide.with(applicationContext)
             .load(screenState.track.artworkUrl100?.replaceAfterLast('/', "512x512bb.jpg"))
+            .transform(RoundedCorners(8))
             .placeholder(R.drawable.placeholder_full_size).into(binding.apTrackImage)
 
         binding.apTrackName.text = screenState.track.trackName
