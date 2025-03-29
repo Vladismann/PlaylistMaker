8fd651664b98d2a152efab5ca0d0f62a006fdafe
Vlad <vladismann@yandex.ru>
2025-03-29 22:50:48 +0300

    feat: TabLayout верстка draft

--- app/src/main/java/com/example/playlistmaker/media/ui/MediaActivity.kt
+++ app/src/main/java/com/example/playlistmaker/media/ui/MediaActivity.kt
@@ -2,9 +2,19 @@ package com.example.playlistmaker.media.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.playlistmaker.databinding.ActivityMediaBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MediaActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMediaBinding
+
+    private lateinit var tabMediator: TabLayoutMediator
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMediaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
